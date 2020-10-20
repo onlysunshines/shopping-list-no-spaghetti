996bd62be42f651b39f9ac5b82d0a0ff196e2d81
Tomás Mier <68940061+onlysunshines@users.noreply.github.com>
2020-10-20 10:56:19 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,4 @@
 
-/*
-//add new item.
-function shoppingListAddItem() {
-    $('#js-shopping-list-form').submit(function (event) {
-      event.preventDefault()
-  
-      //holds the value of item entered in listItem
-      const listItem = $('#shopping-list-entry').val()
-      //if listItem does not equal nothing adds it within the html with all the formating.
-      if (listItem !== "") {
-        $('.shopping-list').append(`<li>
-            <span class='shopping-item'>${listItem}</span>
-            <div class='shopping-item-controls'>
-              <button class='shopping-item-toggle'>
-                <span class='button-label'>check</span>
-              </button>
-              <button class='shopping-item-delete'>
-                <span class='button-label'>delete</span>
-              </button>
-            </div>
-          </li>
-          `)
-      }
-      //resets the form input
-      $('input[name=shopping-list-entry').val('')
-    })
-  }
-  */
-
 let store = [
   { name:'bread', completed:false },
   { name:'milk', completed:false },
